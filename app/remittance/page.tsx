cadfc1fd86eccddc588cdeeec525f27a00856db2
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-07-10 16:34:54 +0630

    Add remittance page

---
+++ app/remittance/page.tsx
@@ -0,0 +1,6 @@
+import Shell from "@/components/shell";
+import PremiumWorkspace from "@/components/premium-workspace";
+
+export default function RemittancePage() {
+  return <Shell title="ငွေလွှဲ/ငွေထုတ်"><PremiumWorkspace view="remittance" /></Shell>;
+}
