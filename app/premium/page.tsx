8517e8491397c98b375a910f776c37477c2d4272
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-07-10 16:05:04 +0630

    Add premium wallet workspace page

---
+++ app/premium/page.tsx
@@ -0,0 +1,10 @@
+import Shell from "@/components/shell";
+import PremiumWorkspace from "@/components/premium-workspace";
+
+export default function PremiumPage() {
+  return (
+    <Shell title="Wallet Note Premium">
+      <PremiumWorkspace />
+    </Shell>
+  );
+}
