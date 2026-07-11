7c934fb21598c7ed2f1e1d8ad1f7704153927eb1
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-07-11 16:40:34 +0630

    Use combined wallet screen

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -1,1 +1,10 @@
-import Shell from "@/components/shell";import WalletClient from "@/components/wallet-client";export default function Page(){return <Shell title="Wallet Management"><WalletClient/></Shell>}
+import Shell from "@/components/shell";
+import MoneyClient from "@/components/money-client";
+
+export default function Page() {
+  return (
+    <Shell title="Wallet">
+      <MoneyClient />
+    </Shell>
+  );
+}
