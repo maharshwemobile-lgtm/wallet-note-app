1dda15e61712bbe5771e897cc19fa22aaf934729
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-07-12 10:28:14 +0630

    Add complete Express entrypoint

---
+++ app-server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+dotenv.config({ path: ".env.production" });
+dotenv.config();
+const [{ SYSTEM_SHEET_ID }, { SYSTEM_TABS, ensureTabs }, { accountRoutes }, { walletBetRoutes }, { debtExchangeRoutes }] = await Promise.all([
+  import("./src/sheets-client.js"),
